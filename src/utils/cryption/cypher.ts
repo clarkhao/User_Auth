dd24d8865c73c4567e88716cd8ea5f2a37e2a55b
clarkhao <clarktotoro@gmail.com>
2023-03-02 09:32:10 +0800

    little

--- src/utils/cryption/cypher.ts
+++ src/utils/cryption/cypher.ts
@@ -14,7 +14,7 @@ function encrypt(data: string, secret: string) {
     const iv = crypto.randomBytes(16);
     const cipher = crypto.createCipheriv('aes-256-ctr', secret, iv);
     const encrypted = Buffer.concat([cipher.update(data), cipher.final()]);
-    return iv.toString('hex') + ':' + encrypted.toString('hex');
+    return iv.toString('hex') + '++' + encrypted.toString('hex');
   } catch (error) {
     logger.warn({ error, location: 'from utils/cryption/cypoher/encrypt' });
     throw new Error(`${error}`);
@@ -25,7 +25,7 @@ function encrypt(data: string, secret: string) {
 */
 function decrypt(data: string, secret: string) {
   try {
-    const [ivHex, encryptedHex] = data.split(':');
+    const [ivHex, encryptedHex] = data.split('++');
     const iv = Buffer.from(ivHex, 'hex');
     const encrypted = Buffer.from(encryptedHex, 'hex');
     const decipher = crypto.createDecipheriv('aes-256-ctr', secret, iv);
